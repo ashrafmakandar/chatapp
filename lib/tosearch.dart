48dda0f5883a5b48884d5bb8865b0d47d3fd1343
ashraf9587 <ashraf9587@gmail.com>
2021-02-26 16:32:30 +0530

    added search bar with functionality

--- lib/tosearch.dart
+++ lib/tosearch.dart
@@ -14,59 +14,104 @@ class _tosearchState extends State<tosearch> {
     Post(name: "hitesh", work: "laravel developer"),
     Post(name: "karthik", work: "web developer"),
     Post(name: "vivek", work: "devops developer"),
+    Post(name: "vivian", work: "kuberenetes developer"),
   ];
   List<Post> search = [];
+
+  var namecontroller = TextEditingController();
+  bool visible = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     print(Allposts.length);
-    var namecontroller = TextEditingController();
+
     return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
             child: TextFormField(
-              controller: namecontroller,
-              onChanged: (val) {
-                Allposts.map((e) => {
-                      if (e.name.contains(val))
-                        {
-                          search.add(new Post(name: e.name, work: "asibfb")),
-                          print("list"+search.toString())
-                        }
-                    });
+              onChanged: (value) {
+                setState(() {
+                  visible = true;
+                });
               },
+              controller: namecontroller,
+              decoration: InputDecoration(
+                  labelText: "ENTER NAME ",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5))),
             ),
           ),
-          FlatButton(
-              color: Colors.blue,
-              onPressed: ()=> {
-              print(search.toString())
-              },
-              child: Text("search")),
-          ListView.builder(
-              shrinkWrap: true,
-              physics: AlwaysScrollableScrollPhysics(),
-              itemCount: Allposts.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Card(
-                      child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(Allposts[index].name),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(Allposts[index].work),
-                      )
-                    ],
-                  )),
-                );
-              }),
+          if (visible)
+            Visibility(
+              visible: visible,
+              child: Container(
+                  height: 500,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: Allposts.length,
+                      itemBuilder: (context, index) {
+                        if (namecontroller.text.isEmpty) {
+                          return Padding(
+                            padding: const EdgeInsets.only(left: 20,right: 20),
+                            child: Card(
+                              color: Colors.blueGrey[100],
+                              child: ListTile(
+                                title: Text(Allposts[index].name),
+                                subtitle: Text(Allposts[index].work),
+                              ),
+                            ),
+                          );
+                        } else if (Allposts[index]
+                            .name
+                            .contains(namecontroller.text)) {
+                          return Card(
+                            color: Colors.blueGrey[100],
+                            child: ListTile(
+                              title: Text(Allposts[index].name),
+                              subtitle: Text(Allposts[index].work),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      })),
+            )
+          else
+            Visibility(
+              visible: true,
+              child: Container(
+                height: 500,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: AlwaysScrollableScrollPhysics(),
+                    itemCount: Allposts.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: Card(
+                            child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(Allposts[index].name),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(Allposts[index].work),
+                            )
+                          ],
+                        )),
+                      );
+                    }),
+              ),
+            ),
         ],
       ),
     );
